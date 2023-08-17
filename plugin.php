9a9fe1525ab4c9cdda518d0984730d54469516e3
vikasiwp <108919165+vikasiwp@users.noreply.github.com>
2023-08-17 18:42:49 +0530

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@ Version: 1.1.0
 */
 
 function hello_world_display() {
-  echo "Hello World using a template changed by Vikas 17 aug";
+  echo "Hello World using a template changed by Vikas 17 aug Push 1";
 }
 
 add_action( 'wp_footer', 'hello_world_display' );
